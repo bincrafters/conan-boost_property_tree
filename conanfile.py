f195ad2864e8b7fc231ceac2d18c1d3d4559af2f
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:34 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,25 +3,25 @@ from conans import ConanFile
 
 class BoostProperty_TreeConan(ConanFile):
     name = "Boost.Property_Tree"
-    version = "1.65.1"
+    version = "1.66.0"
 
     requires = \
-        "Boost.Any/1.65.1@bincrafters/testing", \
-        "Boost.Assert/1.65.1@bincrafters/testing", \
-        "Boost.Bind/1.65.1@bincrafters/testing", \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Format/1.65.1@bincrafters/testing", \
-        "Boost.Iterator/1.65.1@bincrafters/testing", \
-        "Boost.Mpl/1.65.1@bincrafters/testing", \
-        "Boost.Multi_Index/1.65.1@bincrafters/testing", \
-        "Boost.Optional/1.65.1@bincrafters/testing", \
-        "Boost.Range/1.65.1@bincrafters/testing", \
-        "Boost.Serialization/1.65.1@bincrafters/testing", \
-        "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-        "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
-        "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-        "Boost.Utility/1.65.1@bincrafters/testing"
+        "Boost.Any/1.66.0@bincrafters/testing", \
+        "Boost.Assert/1.66.0@bincrafters/testing", \
+        "Boost.Bind/1.66.0@bincrafters/testing", \
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Format/1.66.0@bincrafters/testing", \
+        "Boost.Iterator/1.66.0@bincrafters/testing", \
+        "Boost.Mpl/1.66.0@bincrafters/testing", \
+        "Boost.Multi_Index/1.66.0@bincrafters/testing", \
+        "Boost.Optional/1.66.0@bincrafters/testing", \
+        "Boost.Range/1.66.0@bincrafters/testing", \
+        "Boost.Serialization/1.66.0@bincrafters/testing", \
+        "Boost.Static_Assert/1.66.0@bincrafters/testing", \
+        "Boost.Throw_Exception/1.66.0@bincrafters/testing", \
+        "Boost.Type_Traits/1.66.0@bincrafters/testing", \
+        "Boost.Utility/1.66.0@bincrafters/testing"
 
     lib_short_names = ["property_tree"]
     is_header_only = True
@@ -29,9 +29,9 @@ class BoostProperty_TreeConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-property_tree"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     exports = "boostgenerator.py"
 
